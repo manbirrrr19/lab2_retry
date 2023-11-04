d506e16414876785a3f3a110de794231b0bb2abf
Manbir <manbirjit.togar@gmail.com>
2023-11-05 01:32:14 +0800

    change to bmi.py

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height, weight):
     print("BMI= " + str(round(bmi, 2)))
     if bmi < 18.5:
         print("Underweight")
+        weightclass = -1
     elif 18.5 <= bmi <= 25:
         print("Normal weight")
+        weightclass = 0
     elif bmi > 25:
         print("Overweight")
+        weightclass = 1
 
 
 calculate_bmi(weight=57, height=1.73)
